47d52a446120e0381c93d79d069c8ace78304913
Harsh <85244058+blackstargamer@users.noreply.github.com>
2022-06-12 08:46:29 +0530

    Add files via upload

---
+++ para/script.js
@@ -0,0 +1,18 @@
+console.log("Hi There!");
+
+// let str1 = "string first";
+// let str2 = "stringSecond";
+// console.log(str1 + " " + str2); 
+// console.log(str2.toUpperCase());
+// console.log(str2.toLowerCase());
+// console.log(str1.replace("string second"));
+
+// let arr = [1,2,4,8,12,16,20,24,28,32,36,40];
+// console.log(arr);
+// arr.toString();
+// arr.splice(2,10);
+// arr();
+// console.log(arr);
+
+let elem = document.querySelector("param");
+console.log(elem);
